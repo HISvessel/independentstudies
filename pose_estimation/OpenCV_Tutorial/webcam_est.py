7996ddab19f46e8ed9e6dc7ef68e2f8800ec9b76
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-02 13:33:16 -0400

    added chages

--- pose_estimation/OpenCV_Tutorial/webcam_est.py
+++ pose_estimation/OpenCV_Tutorial/webcam_est.py
@@ -4,14 +4,13 @@ a practical example to apply. This module intends to employ webcam face detectio
 import cv2
 
 # Load pre-trained face detector
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+CASCADE_PATH = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+CASCADE_PROFILE = cv2.data.haarcascades + "haarcascade_profileface.xml"
+face_cascade = cv2.CascadeClassifier(CASCADE_PATH)
+profile_cascade = cv2.CascadeClassifier(CASCADE_PROFILE)
 
 # Open webcam
-# the webcam is tied to a telephone via DroidCam
-#vchanges are pending, so that a phone will not be used for the final
-# project
-cap = cv2.VideoCapture("http://192.168.9.177:4747/video")
-#cap = cv2.VideoCapture(0) #this would change dependin on the situation
+cap = cv2.VideoCapture("http://192.168.0.15:4747/video") #set to WiFI IP address
 while True:
     ret, frame = cap.read()  # Capture frame
     if not ret:
@@ -20,14 +19,17 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
 
     # Detect faces
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100,100))
+    profiles = profile_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30))
 
     # Draw rectangles around detected faces
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+    
+    for (w, y, w, h) in profiles:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 1)
 
     cv2.imshow("Webcam Face Detection", frame)
-
     # Break on 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
