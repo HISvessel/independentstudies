9700808f85441be8bd8595499127f30619eb5c4f
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-03 14:26:24 -0400

    moving and organizing files for greater separation of concerns

--- pose_estimation/mp_intro.py
+++
@@ -1,8 +0,0 @@
-"""this code snippet is our introduction into mediapipe: a 
-cross platform framework designed to building perceptual pipelines, 
-good for tasks like pose estimation, face landmarking, object tracking, 
-etc. 
-
-In our future project, this framework will detect human body keypoints
-as the foundation for form analysis."""
-
