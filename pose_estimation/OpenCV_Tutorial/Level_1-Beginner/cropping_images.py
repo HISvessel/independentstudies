18367ad6eee5b5517f08fca45249a277544c7e54
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-06 13:30:32 -0400

    added control flow logic, to perform actions as keys are pressed on the keyboard

--- pose_estimation/OpenCV_Tutorial/Level_1-Beginner/cropping_images.py
+++ pose_estimation/OpenCV_Tutorial/Level_1-Beginner/cropping_images.py
@@ -10,6 +10,19 @@ img = cv2.imread("single-line.jpg")
 cropped = img[50:200, 100:300]
 
 cv2.imshow("Original", img)
-cv2.imshow("Cropped", cropped)
-cv2.waitKey(0)
+
+#runs a process that maps keyboard to perform cropping and quitting
+while True:
+    key = cv2.waitKey(0) & 0XFF
+
+    #if c is pressed on the keyboard, then show the image
+    #this process is run once, so no extra images pop into the screen if I re-press it by accident
+    if key ==  ord('c'):
+        cv2.imshow("Cropped", cropped)
+        cv2.imshow('Second cropped', cropped)
+
+    #if q is pressed on the keyboard, end the loop
+    elif key == ord('q'):
+        break
+
 cv2.destroyAllWindows()
