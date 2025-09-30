2a47909b1b91690a19d5526d9f1ebc70c1047652
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-30 11:49:02 -0400

    log messages written for server side solutions. Preparing to debug current error: request is being recieved but the desired decoded frame responses are not being sent for the video feed to output on the div.

--- Projects/Phase_2/Mini_App/app/__init__.py
+++ Projects/Phase_2/Mini_App/app/__init__.py
@@ -1,11 +1,25 @@
 """this script runs the application"""
 from flask import Flask
-from app.classes.threaded_cam import CameraThreaded
-from app.routes.camera_routes import camera_blueprint
+#from app.routes.camera_routes import camera_blueprint
+import logging
 
 
 def create_app():
+    from app.routes.camera_routes import camera_blueprint
+
     app = Flask(__name__, template_folder='fhtml')
     app.register_blueprint(camera_blueprint)
+    #basic configuration for message logs
+    logging.basicconfig(
+        level=logging.DEBUG,
+        format='%(asctime)S [%(levelname)S] $(message)S'
+    )
+    logger = logging.getLogger(__name__)
+
+    #logging error handling exceptions
+    @app.errorhandler(Exception)
+    def handle_exception(e):
+        logger.error(f'Unhandled eception: {e}', exc_info=True)
+        return {"Error": str(e)}, 500
 
     return app
--- Projects/Phase_2/Mini_App/app/routes/camera_routes.py
+++ Projects/Phase_2/Mini_App/app/routes/camera_routes.py
@@ -3,27 +3,38 @@
 from flask import Flask, Blueprint, render_template, Response, send_file, jsonify
 from app.classes.threaded_cam import CameraThreaded
 import io
+import logging
 
 
 camera_blueprint = Blueprint('camera', __name__)
-camera = CameraThreaded('http://192.168.9.175:4747/video')
+camera = CameraThreaded('http://192.168.9.175:4747/video').start()
+logger = logging.getLogger(__name__)
+
 
 @camera_blueprint.route('/')
 def index():
+    logger.info('Serving webpage')
     return render_template('index.html')
 
-@camera_blueprint.route('/vid')
+@camera_blueprint.route('/video_feed')
 def video_feed():
-    def generate():
-        while True:
-            frame_bytes = camera.get_encoded_frame()
-            if frame_bytes:
-                 yield (b'--frame\r\n'
+    logger.info('Client requesting camera service')
+    try:
+        def generate():
+            while True:
+                frame_bytes = camera.get_encoded_frame()
+                if frame_bytes:
+                    yield (b'--frame\r\n'
                        b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-    return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    except Exception as e:
+        logger.error(f'Service could not be provided: {e}', exc_info=True)
+        return {"Error": str(e)}, 500
+
 
 @camera_blueprint.route('/snapshot')
 def take_snapshot():
+    logger.info("Snapshot requested")
     frame_bytes = camera.get_encoded_frame()
     return send_file(io.BytesIO(frame_bytes), mimetype='image/jpeg', download_name='snapshot.jpg')
 
