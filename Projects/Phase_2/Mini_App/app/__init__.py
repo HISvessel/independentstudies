c942f361a7555f73e8dfa35575ed9615c3b1e8a0
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-30 10:13:49 -0400

    registering the camera blueprints to the create app function. Preparing deployment.

--- Projects/Phase_2/Mini_App/app/__init__.py
+++ Projects/Phase_2/Mini_App/app/__init__.py
@@ -1,7 +1,11 @@
 """this script runs the application"""
 from flask import Flask
 from app.classes.threaded_cam import CameraThreaded
+from app.routes.camera_routes import camera_blueprint
+
 
 def create_app():
     app = Flask(__name__, template_folder='fhtml')
+    app.register_blueprint(camera_blueprint)
+
     return app
