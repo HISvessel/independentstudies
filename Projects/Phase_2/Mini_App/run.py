d4627dd74360890d505afe4e12c4e7517097bdc5
HISvessel <kevinsanchezdabest@yahoo.com>
2025-10-06 12:21:14 -0400

    changed IP address tempoarily

--- Projects/Phase_2/Mini_App/run.py
+++ Projects/Phase_2/Mini_App/run.py
@@ -9,7 +9,7 @@ camera_source = 'http://192.168.9.175:4747/video'
 
 if __name__ == '__main__':
     try:
-        camera = CameraThreaded(source='http://192.168.0.9:4747/video')
+        camera = CameraThreaded(source='http://192.168.9.175:4747/video')
         socketio.start_background_task(video_feed, socketio, camera)
         socketio.run(app, host='0.0.0.0', port=5000, debug=True, use_reloader=False)
     
